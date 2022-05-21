e52834c1ad719bb2523cceab4fbc6d35b01ef189
Jasmeet-1998 <devs.us.1984@gmail.com>
2022-05-21 12:11:15 +0530

    ✔: chapter-1 variable & Data Types

--- Basics/initiate.go
+++ Basics/initiate.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello go!")
+	// will overflow as uint8 cannot denote 260
+	// var number uint8 = 260
+	var number uint = 100023345
+	var number2 uint16 = 260
+	fmt.Println("Hello go!", number, number2)
 }
