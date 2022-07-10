b6ecaa835313c02c9156a91dcd731b56c6c7cabf
Jasmeet-1998 <devs.us.1984@gmail.com>
2022-07-10 18:59:36 +0530

    services setup models & db interaction CRUD

--- books-management-api/pkg/models/book.go
+++ books-management-api/pkg/models/book.go
@@ -21,3 +21,36 @@ func init() {
 	// migrate the Book schema/struct to the DB
 	db.AutoMigrate(&Book{})
 }
+
+/*Services for models to interact with database*/
+
+// CREATE new book
+func (newbook *Book) CreateBook() *Book {
+	// sql query for creating new book handled by gorm by default no need to write queries
+	db.NewRecord(newbook)
+	// use newbook value to create new book entry
+	db.Create(&newbook)
+	return newbook
+}
+
+// GET all books
+func GetAllBooks() []Book {
+	var Books []Book
+	// grab all the books and store in Books slice var of type Book struct
+	db.Find(&Books)
+	return Books
+}
+
+// GET a book by id
+func GetBookById(Id int64) (*Book, *gorm.db) {
+	var getBook Book
+	db := db.Where("ID=?", Id).Find(&getBook)
+	// return both the book and the db instance of gorm.db
+	return &getBook, db
+}
+
+func DeleteBook(ID int64) Book {
+	var book Book
+	db.Where("ID=?", ID).Delete(book)
+	return book
+}
