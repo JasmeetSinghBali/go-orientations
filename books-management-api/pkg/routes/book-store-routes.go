8d550047e6f1ee5844081a42a92a96991b753863
Jasmeet-1998 <devs.us.1984@gmail.com>
2022-07-10 18:00:04 +0530

    mux-router: barebones setup done

--- books-management-api/pkg/routes/book-store-routes.go
+++ books-management-api/pkg/routes/book-store-routes.go
@@ -1,1 +1,15 @@
 package routes
+
+// absolute path imports
+import (
+	"github.com/Jasmeet-1998/go-orientations/books-management-api/pkg/controllers"
+	"gitub.com/gorilla/mux"
+)
+
+var RegisterBookStoreRoutes = func(router *mux.Router) {
+	router.HandleFunc("/book/", controllers.CreateBook).Method("POST")
+	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.UpdateBookById).Methods("PUT")
+	router.HandleFunc("/book/{bookId}", controllers.DeleteBookById).Methods("DELETE")
+}
