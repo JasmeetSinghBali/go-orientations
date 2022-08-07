817331001c3a74fc9306c0b56f0305033a67dc79
Jasmeet-1998 <devs.us.1984@gmail.com>
2022-08-07 12:49:19 +0530

    setup: auth&user routes,controller,models

--- go_rest_jwt_gin_mongodb/routes/user.router.go
+++ go_rest_jwt_gin_mongodb/routes/user.router.go
@@ -1,1 +1,14 @@
 package routes
+
+import(
+	controller "go_rest_jwt_gin_mongodb/controllers"
+	middlewares "go_rest_jwt_gin_mongodb/middlewares"
+	"github.com/gin-gonic/gin"
+)
+
+/*make all the user routes protected via middleware*/
+func UserRoutes(incomingRoutes *gin.Engine){
+	incomingRoutes.Use(middlewares.Authenticate())
+	incomingRoutes.GET("/users", controller.GetUsers())
+	incomingRoutes.GET("/users/:user_id",controller.GetUser())
+}
