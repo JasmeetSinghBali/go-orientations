8858c0eb220072f1509347848898c16f0834be7a
JasmeetSinghBali <devs.us.1984@gmail.com>
2023-09-15 15:42:23 +0530

    sorted: a generic map with process function

---
+++ gophershub/generics_1.18_2022/go.mod
@@ -0,0 +1,5 @@
+module github.com/JasmeetSinghBali/go-orientations/tree/stable/gophershub/generics_1.18_2022
+
+go 1.21.0
+
+require golang.org/x/exp v0.0.0-20230905200255-921286631fa9
--- gophershub/generics_1.18_2022/main.go
+++ gophershub/generics_1.18_2022/main.go
@@ -1,6 +1,31 @@
 package main
 
-// 🎈 a generic mapping function
-func main() {
+import (
+	"fmt"
 
+	"golang.org/x/exp/constraints"
+)
+
+// 💫 a generic mapping function
+// input: [1,2,3],(n) => n*2
+// output: [2,4,6]
+// NOTE- constraints.Ordered contain all the int & string types
+func MapItOver[T constraints.Ordered](val []T, processFunc func(T) T) []T {
+	var newValues []T
+	for _, v := range val {
+		newValue := processFunc(v)
+		newValues = append(newValues, newValue)
+	}
+	return newValues
+}
+
+func main() {
+	resultInt := MapItOver([]int{1, 2, 3}, func(n int) int {
+		return n * 7
+	})
+	fmt.Println(resultInt)
+	resultFloat := MapItOver([]float64{1.2, 2.3, 3.1}, func(n float64) float64 {
+		return n * 7
+	})
+	fmt.Println(resultFloat)
 }
